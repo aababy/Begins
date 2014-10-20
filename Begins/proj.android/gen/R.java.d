f8ece2aee21d619cf3780891984bdb8e73e445c3
luoxp <jumpchess@163.com>
2014-10-20 16:28:45 +0800

    i

--- Begins/proj.android/gen/R.java.d
+++
@@ -1,7 +0,0 @@
-/Users/luoxp/ResearchProjects/Begins/Begins/proj.android/gen/com/luoxp/begins/R.java \
- : /Users/luoxp/ResearchProjects/Begins/Begins/proj.android/res/drawable-hdpi/icon.png \
-/Users/luoxp/ResearchProjects/Begins/Begins/proj.android/res/drawable-ldpi/icon.png \
-/Users/luoxp/ResearchProjects/Begins/Begins/proj.android/res/drawable-mdpi/icon.png \
-/Users/luoxp/ResearchProjects/Begins/Begins/proj.android/res/raw/abc.mp3 \
-/Users/luoxp/ResearchProjects/Begins/Begins/proj.android/res/values/strings.xml \
-/Users/luoxp/ResearchProjects/Begins/Begins/proj.android/bin/AndroidManifest.xml \
--- Begins/proj.android/gen/com/luoxp/begins/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.luoxp.begins;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
